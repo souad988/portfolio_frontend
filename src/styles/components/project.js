2a7aab6169b5461d37ca43ef57cf4621b36e91b0
souad988 <souadelmansouri88@gmail.com>
2023-08-10 21:46:57 +0100

    Add project component and reducer

---
+++ src/styles/components/project.js
@@ -0,0 +1,10 @@
+export const styles = (theme) => ({
+    container: {
+        backgroundColor: theme.backgroundColor,
+        boxShadow:'rgba(0, 179, 159, 0.2) 0px 10px 25px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem 2rem ',
+    }
+  })
